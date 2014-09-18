7ef781085a3bef9ec361cc6307e95984298ba623
Lukasz Chromy <lukasz.chromy@miquido.com>
2014-09-18 17:11:24 +0200

    Initial commit

---
+++ app/src/main/java/com/sayler/lpreview/CircularReveal.java
@@ -0,0 +1,238 @@
+/**
+ * Created by Lukasz Chromy on 13.01.14.
+ *
+ * Copyright 2014 MiQUiDO <http://www.miquido.com/>. All rights reserved.
+ */
+package com.sayler.lpreview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.*;
+import android.graphics.Paint.Style;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.OvershootInterpolator;
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.ValueAnimator;
+
+public class CircularReveal extends View implements ValueAnimator.AnimatorUpdateListener, Animator.AnimatorListener {
+
+  private Mode MODE = Mode.HIDE;
+
+  private int duration = 2000;
+  private int progress = 100;
+
+  //Sizes (with defaults)
+  private int layoutHeight = 0;
+  private int layoutWidth = 0;
+  private int longerSide;
+
+  //Colors (with defaults)
+  private int barColor = 0xAA000000;
+
+  //Paints
+  private Paint barPaint = new Paint();
+  private Paint fillPaint;
+  private Paint clearPaint;
+  private Paint defaultPaint;
+
+  //rect
+  private RectF fullCircle = new RectF();
+
+  //bitmaps canvas
+  private Bitmap tmpBitmap;
+  private Canvas tmpCanvas;
+
+  private Handler spinHandler = new Handler() {
+
+    @Override
+    public void handleMessage(Message msg) {
+      invalidate();
+    }
+  };
+
+  /**
+   * The constructor for the ProgressWheel
+   *
+   * @param context
+   * @param attrs
+   */
+  public CircularReveal(Context context, AttributeSet attrs) {
+    super(context, attrs);
+
+    parseAttributes(context.obtainStyledAttributes(attrs,
+        R.styleable.CircularReveal));
+  }
+
+  //----------------------------------
+  //Setting up stuff
+  //----------------------------------
+
+  @Override
+  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    super.onSizeChanged(w, h, oldw, oldh);
+
+    // Share the dimensions
+    layoutWidth = w;
+    layoutHeight = h;
+    longerSide = layoutWidth;
+    if (layoutHeight > layoutWidth) {
+      longerSide = layoutHeight;
+    }
+
+    setupBounds();
+    setupPaints();
+    invalidate();
+  }
+
+  private void setupPaints() {
+    barPaint.setColor(barColor);
+    barPaint.setAntiAlias(true);
+    barPaint.setStyle(Style.FILL);
+
+    clearPaint = new Paint();
+    clearPaint.setAntiAlias(true);
+    clearPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+
+    fillPaint = new Paint();
+    fillPaint.setAntiAlias(true);
+    fillPaint.setColor(barPaint.getColor());
+
+    defaultPaint = new Paint();
+    defaultPaint.setAntiAlias(true);
+
+    tmpBitmap = Bitmap.createBitmap(layoutWidth, layoutHeight, Bitmap.Config.ARGB_8888);
+    tmpCanvas = new Canvas(tmpBitmap);
+  }
+
+  /**
+   * Set the bounds of the component
+   */
+  private void setupBounds() {
+    fullCircle = new RectF(layoutWidth / 2 - longerSide, layoutHeight / 2 - longerSide, layoutWidth / 2 + longerSide, layoutHeight / 2 + longerSide);
+  }
+
+  /**
+   * Parse the attributes passed to the view from the XML
+   *
+   * @param a the attributes to parse
+   */
+  private void parseAttributes(TypedArray a) {
+
+    barColor = a.getColor(R.styleable.CircularReveal_barColor, barColor);
+
+    a.recycle();
+  }
+
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+    fullCircle.left = layoutWidth / 2 - longerSide * (getProgress() / 100f);
+    fullCircle.top = layoutHeight / 2 - longerSide * (getProgress() / 100f);
+    fullCircle.right = layoutWidth / 2 + longerSide * (getProgress() / 100f);
+    fullCircle.bottom = layoutHeight / 2 + longerSide * (getProgress() / 100f);
+    switch (MODE) {
+      case REVEAL:
+
+        tmpCanvas.drawRect(0, 0, tmpCanvas.getWidth(), tmpCanvas.getHeight(), fillPaint);
+        tmpCanvas.drawOval(fullCircle, clearPaint);
+        canvas.drawBitmap(tmpBitmap, 0, 0, defaultPaint);
+        break;
+      case HIDE:
+        canvas.drawOval(fullCircle, barPaint);
+        break;
+    }
+  }
+
+  public void resetCount() {
+    setProgress(0);
+    invalidate();
+  }
+
+  public void stopSpinning() {
+    setProgress(0);
+    spinHandler.removeMessages(0);
+  }
+
+  /**
+   * Puts the view on spin MODE
+   */
+  public void spin() {
+    spinHandler.sendEmptyMessage(0);
+  }
+
+  /**
+   * Increment the progress by 1 (of 360)
+   */
+  public void incrementProgress() {
+    setProgress(getProgress() + 1);
+    if (getProgress() > 360) {
+      setProgress(0);
+    }
+    spinHandler.sendEmptyMessage(0);
+  }
+
+  public void setProgress(int i) {
+    //value animator
+    progress = i;
+    invalidate();
+  }
+
+  public void hide(boolean withAnimation) {
+    MODE = Mode.HIDE;
+    startAnimation(withAnimation);
+  }
+
+  public void reveal(boolean withAnimation) {
+    MODE = Mode.REVEAL;
+    startAnimation(withAnimation);
+  }
+
+  private void startAnimation(boolean withAnimation) {
+    if (withAnimation) {
+      ValueAnimator animation = ValueAnimator.ofInt(0, 100);
+      animation.setDuration(duration);
+      animation.start();
+      animation.setInterpolator(new OvershootInterpolator());
+      animation.addUpdateListener(this);
+    } else {
+      setProgress(100);
+    }
+  }
+
+  @Override
+  public void onAnimationUpdate(ValueAnimator valueAnimator) {
+    setProgress((Integer) valueAnimator.getAnimatedValue());
+    spinHandler.sendEmptyMessage(0);
+  }
+
+  @Override
+  public void onAnimationStart(Animator animation) {
+
+  }
+
+  @Override
+  public void onAnimationEnd(Animator animation) {
+
+  }
+
+  @Override
+  public void onAnimationCancel(Animator animation) {
+
+  }
+
+  @Override
+  public void onAnimationRepeat(Animator animation) {
+
+  }
+
+  public int getProgress() {
+    return progress;
+  }
+
+  public enum Mode {
+    REVEAL, HIDE;
+  }
+
+}
---
+++ app/src/main/java/com/sayler/lpreview/Main.java
@@ -0,0 +1,146 @@
+package com.sayler.lpreview;
+
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.LayoutTransition;
+import android.app.Activity;
+import android.app.Fragment;
+import android.graphics.Outline;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.OvershootInterpolator;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+public class Main extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);
+    setContentView(R.layout.activity_main);
+    if (savedInstanceState == null) {
+      getFragmentManager().beginTransaction()
+          .add(R.id.container, new PlaceholderFragment())
+          .commit();
+    }
+  }
+
+  @Override
+  public void finish() {
+    super.finish();
+    overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);
+  }
+
+  public class PlaceholderFragment extends Fragment {
+
+    private boolean center;
+    private AnimatorSet colorChange;
+
+    public PlaceholderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+      final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+      final ImageButton addButton = (ImageButton) rootView.findViewById(R.id.add_button);
+      final ImageButton addButton1 = (ImageButton) rootView.findViewById(R.id.add_button_1);
+      final int diameter = getResources().getDimensionPixelSize(R.dimen.diameter);
+      final CircularReveal progress_bar = (CircularReveal) rootView.findViewById(R.id.progress_bar);
+      Outline outline = new Outline();
+      outline.setOval(0, 0, diameter, diameter);
+      addButton.setOutline(outline);
+      addButton.setClipToOutline(true);
+      addButton1.setOutline(outline);
+      addButton1.setClipToOutline(true);
+
+      LayoutTransition layoutTransition = ((RelativeLayout) rootView).getLayoutTransition();
+      layoutTransition.enableTransitionType(LayoutTransition.CHANGING);
+      layoutTransition.setDuration(1000);
+      layoutTransition.setInterpolator(LayoutTransition.CHANGING, new OvershootInterpolator());
+
+      Animation actionButtonAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.reveal);
+      addButton.startAnimation(actionButtonAnimation);
+
+      colorChange = (AnimatorSet) AnimatorInflater.loadAnimator(getActivity(), R.animator.color);
+      colorChange.setTarget(addButton);
+
+      addButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+          RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) addButton.getLayoutParams();
+          RelativeLayout.LayoutParams layoutParams1 = (RelativeLayout.LayoutParams) addButton1.getLayoutParams();
+
+          if (center) {
+            layoutParams.removeRule(RelativeLayout.CENTER_IN_PARENT);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            layoutParams1.removeRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            rootView.setBackground(getDrawable(android.R.color.transparent));
+            progress_bar.reveal(true);
+            center = false;
+            colorChange.removeAllListeners();
+            addButton.startAnimation(AnimationUtils.loadAnimation(getActivity(), R.anim.rotate));
+            Drawable[] layers = new Drawable[2];
+            layers[0] = getDrawable(android.R.color.transparent);
+            layers[1] = getDrawable(android.R.drawable.ic_delete);
+            TransitionDrawable transition = new TransitionDrawable(layers);
+            addButton.setImageDrawable(transition);
+            transition.startTransition(500);
+          } else {
+            layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+            layoutParams.removeRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+            layoutParams.removeRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            layoutParams1.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            progress_bar.hide(true);
+            center = true;
+            addButton.startAnimation(AnimationUtils.loadAnimation(getActivity(), R.anim.loader));
+            colorChange.start();
+            colorChange.addListener(new Animator.AnimatorListener() {
+              @Override
+              public void onAnimationStart(Animator animator) {
+
+              }
+
+              @Override
+              public void onAnimationEnd(Animator animator) {
+                colorChange.start();
+              }
+
+              @Override
+              public void onAnimationCancel(Animator animator) {
+
+              }
+
+              @Override
+              public void onAnimationRepeat(Animator animator) {
+
+              }
+
+            });
+            Drawable[] layers = new Drawable[2];
+            layers[0] = getDrawable(android.R.color.transparent);
+            layers[1] = getDrawable(android.R.drawable.ic_dialog_info);
+            TransitionDrawable transition = new TransitionDrawable(layers);
+            addButton.setImageDrawable(transition);
+            transition.startTransition(500);
+          }
+          addButton.setLayoutParams(layoutParams);
+
+        }
+      });
+      progress_bar.reveal(false);
+      return rootView;
+    }
+  }
+}
